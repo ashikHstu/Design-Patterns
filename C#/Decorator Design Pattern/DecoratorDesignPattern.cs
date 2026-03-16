96b1f7787803eee3167923f627372aa6c7e20d96
ashikHstu <ashik.cse.hstu@gmail.com>
2026-03-17 03:27:28 +0600

    Decorator pattern ongoing

---
+++ C#/Decorator Design Pattern/DecoratorDesignPattern.cs
@@ -0,0 +1,53 @@
+public interface ICoffee
+{
+    string GetDescription();
+    double GetCost();
+}
+
+public class SimpleCoffee:ICoffee
+{
+    public string GetDescription()
+    {
+        return "Simple coffee,";
+    }
+
+    public bool GetCost()
+    {
+        return 5.0;
+    }
+}
+
+public abstract class CoffeeDecorator:ICoffee
+{
+    protected ICoffee coffee;
+    public CoffeeDecorator(ICoffee coffee)
+    {
+        this.coffee=coffee;
+    }
+
+    public String GetDescription()
+    {
+        return coffee.GetDescription();
+    }
+
+    public int GetCost()
+    {
+        return coffee.GetCost();
+    }
+}
+
+public class MilkDecorator:CoffeeDecorator
+{
+    public MilkDecorator(ICoffee coffee):base(coffee)
+    {
+        
+    }
+    public override string GetDescription()
+    {
+        return this.coffee.GetDescription+",Milk";
+    }
+    public override double GetCost()
+    {
+        return this.coffee.GetCost+0.2;
+    }
+}
