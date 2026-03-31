3f6bd17fad0326c86cf1250f08e3a823af0972a2
ashikHstu <ashik.cse.hstu@gmail.com>
2026-04-01 00:14:54 +0600

    Decorator pattern aded

--- C#/Decorator Design Pattern/DecoratorDesignPattern.cs
+++ C#/Decorator Design Pattern/DecoratorDesignPattern.cs
@@ -11,7 +11,7 @@ public class SimpleCoffee:ICoffee
         return "Simple coffee,";
     }
 
-    public bool GetCost()
+    public double GetCost()
     {
         return 5.0;
     }
@@ -25,12 +25,12 @@ public abstract class CoffeeDecorator:ICoffee
         this.coffee=coffee;
     }
 
-    public String GetDescription()
+    public virtual String GetDescription()
     {
         return coffee.GetDescription();
     }
 
-    public double GetCost()
+    public virtual double GetCost()
     {
         return coffee.GetCost();
     }
@@ -44,10 +44,46 @@ public class MilkDecorator:CoffeeDecorator
     }
     public override string GetDescription()
     {
-        return this.coffee.GetDescription+",Milk";
+        return this.coffee.GetDescription()+",Milk";
     }
     public override double GetCost()
     {
-        return this.coffee.GetCost+0.2;
+        return this.coffee.GetCost()+0.2;
+    }
+}
+
+public class SugerDecoator:CoffeeDecorator
+{
+    public SugerDecoator(ICoffee coffee):base(coffee)
+    {
+        
+    }
+    
+    public override string GetDescription()
+    {
+        return this.coffee.GetDescription()+", Suger";
+    }
+
+    public override double GetCost()
+    {
+        return this.coffee.GetCost()+0.1;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ICoffee coffee = new SimpleCoffee();
+        Console.WriteLine(coffee.GetDescription());
+        Console.WriteLine($"Total cost : {coffee.GetCost()}");
+
+        coffee = new MilkDecorator(coffee);
+        Console.WriteLine(coffee.GetDescription());
+        Console.WriteLine($"Total cost : {coffee.GetCost()}");
+
+        coffee = new SugerDecoator(coffee);
+        Console.WriteLine(coffee.GetDescription());
+        Console.WriteLine($"Total cost : {coffee.GetCost()}");
     }
 }
