e830594c363f1ccfb2e8ee8a5e379945bac44f12
ashikHstu <ashik.cse.hstu@gmail.com>
2026-04-14 23:50:35 +0600

    Template method class added

---
+++ C#/Template Method Pattern/TemplateMethodPattern.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel;
+
+public abstract class Beverage
+{
+    // Template Method (defines the algorithm)
+    public void PrepareRecipe()
+    {
+        BoilWater();
+        Brew();
+        PourInCup();
+        AddCondiments();
+    }
+
+    protected void BoilWater()
+    {
+        Console.WriteLine("Boiling Water...");
+    }
+
+    protected abstract void Brew(); // must override
+
+    protected void PourInCup()
+    {
+        Console.WriteLine("Pouring into cup...");
+    }
+
+    protected abstract void AddCondiments(); // must override
+}
+
