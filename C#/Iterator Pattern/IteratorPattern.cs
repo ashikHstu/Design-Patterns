e17a6d31aaaa6f2da1a17068a3d29b6979cab6c8
ashikHstu <ashik.cse.hstu@gmail.com>
2026-07-12 14:41:21 +0600

    Iterator pattern added

--- C#/Iterator Pattern/IteratorPattern.cs
+++ C#/Iterator Pattern/IteratorPattern.cs
@@ -3,3 +3,70 @@ public interface IIterator<T>
     bool HasNext();
     T Next();
 }
+
+public interface IAggregate<T>
+{
+    IIterator<T> CreateIterator();
+}
+
+public class BookCollection : IAggregate<string>
+{
+    private List<string> books = new List<string>();
+
+    public void AddBook(string book)
+    {
+        books.Add(book);
+    }
+
+    public string GetBook(int index)
+    {
+        return books[index];
+    }
+
+    public int Count => books.Count;
+
+    public IIterator<string> CreateIterator()
+    {
+        return new BookIterator(this);
+    }
+}
+
+public class BookIterator : IIterator<string>
+{
+    private BookCollection collection;
+    private int position = 0;
+
+    public BookIterator(BookCollection collection)
+    {
+        this.collection = collection;
+    }
+
+    public bool HasNext()
+    {
+        return position < collection.Count;
+    }
+
+    public string Next()
+    {
+        return collection.GetBook(position++);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BookCollection books = new BookCollection();
+
+        books.AddBook("Clean Code");
+        books.AddBook("Design Patterns");
+        books.AddBook("Refactoring");
+
+        IIterator<string> iterator = books.CreateIterator();
+
+        while (iterator.HasNext())
+        {
+            Console.WriteLine(iterator.Next());
+        }
+    }
+}
